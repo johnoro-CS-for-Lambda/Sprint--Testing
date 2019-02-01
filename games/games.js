c7eb71673d23d4a333f1092be7ffe3d8920675ec
johnoro <officialjohnorourke@gmail.com>
2019-02-01 13:56:42 -0500

    Make games route handlers more modular and performant

--- games/games.js
+++ games/games.js
@@ -4,69 +4,90 @@ const games = [
   { title: 'Pacman', genre: 'Arcade', releaseYear: 1980, id: id++ }
 ];
 
-const getById = (gameId) => {
-  return games.find(game => game.id == gameId);
+const getById = (gameId) => games.find(({ id }) => id == gameId);
+
+const sendNotFound = res => {
+  res.status(404).json({ message: 'No game found with the given id.' });
 };
 
 const GET = (req, res) => {
-  const gameId = req.params.id;
-  if (!gameId) { // GET
+  const { id } = req.params;
+  if (!id) { // GET
     return res.status(200).json(games);
   }
 
-  if (getById(gameId)) { // GET by id
-    return res.status(200).json(getById(gameId));
+  const game = getById(id);
+
+  if (game) { // GET by id
+    return res.status(200).json(game);
   }
 
-  return res.status(404).json({ message: 'No game found with the given id.' });
+  return sendNotFound(res);
 };
 
 const POST = (req, res) => {
-  const { title, genre } = req.body;
+  const { title, genre, releaseYear } = req.body;
+
   if (!title || !genre) {
-    return res.status(422).json({ message: 'Title and genre are required.' });
+    return res.status(422).json({
+      message: 'Title and genre are required.'
+    });
   }
 
   for (let game of games) {
     if (title === game.title) {
-      return res.status(405).json({ message: 'That game title is already in use.' });
+      return res.status(405).json({
+        message: 'That game title is already in use.'
+      });
     }
   }
 
-  games.push({ title, genre, id });
+  games.push({ title, genre, releaseYear, id });
   return res.status(201).json(id++);
 };
 
 const PUT = (req, res) => {
   const game = getById(req.params.id);
+
   if (!game) {
-    return res.status(404).json({ message: 'No game found with the given id.' });
+    return sendNotFound(res);
   }
 
-  const { title, genre, releaseYear } = req.body;
+  const {
+    title = game.title,
+    genre = game.genre,
+    releaseYear = game.releaseYear
+  } = req.body;
   const { id } = game;
   const updated = { title, genre, releaseYear, id };
-  let delInd;
-  for (let i = 0; i < games.length; i++) {
+
+  let delInd = null, i = 0;
+  do {
     if (id === games[i].id) {
       delInd = i;
     }
+    i++;
   }
+  while (delInd === null);
+
   return res.status(200).json(games.splice(delInd, 1, updated).length);
 };
 
 const DELETE = (req, res) => {
   const game = getById(req.params.id);
   if (!game) {
-    return res.status(404).json({ message: 'No game found with the given id.' });
+    return sendNotFound(res);
   }
 
-  let delInd;
-  for (let i = 0; i < games.length; i++) {
+  let delInd = null, i = 0;
+  do {
     if (game.id === games[i].id) {
       delInd = i;
     }
+    i++;
   }
+  while (delInd === null);
+
   return res.status(202).json(games.splice(delInd, 1).length);
 };
 
